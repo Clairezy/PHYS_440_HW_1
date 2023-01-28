93505026e36dd0ce5102a629ca77ef9186d0ccfa
Clairezy <clairezwicker98@gmail.com>
2023-01-27 19:48:56 -0600

    Initial Commit

---
+++ HW_1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HW_1
+//
+//  Created by Claire on 1/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW_1/HW_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1App.swift
+//  HW_1
+//
+//  Created by Claire on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HW_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
